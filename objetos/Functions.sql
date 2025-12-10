e6a141d56c1d14af9405993fdd3a6b3172b654f5
dcosta0 <161538866+dcosta0@users.noreply.github.com>
2025-12-10 13:01:24 -0300

    Update Functions.sql

--- objetos/Functions.sql
+++ objetos/Functions.sql
@@ -13,3 +13,25 @@ BEGIN
     END IF;
 END;
 $$ LANGUAGE plpgsql;
+
+
+
+/* function que calcula a média de kills por minuto de um jogador em uma partida
+por Daniel da Costa */
+
+create or replace function n.abatespormin(kills int, tempodejogototalsegs int, tempogastomortosegs int)
+returns numeric(10,2) as $$
+declare 
+mediafinal numeric(10,2);
+tempovivo numeric(10,2);
+begin
+    tempovivo := tempodejogototalsegs - tempogastomortosegs;
+    IF tempovivo <= 0 then
+      RAISE EXCEPTION 'Jogador passou o jogo inteiro morto!';
+      return 0;
+    else
+      mediafinal := (kills) / (tempovivo/60);
+      return mediafinal;
+    end if;
+end;
+$$ LANGUAGE plpgsql;

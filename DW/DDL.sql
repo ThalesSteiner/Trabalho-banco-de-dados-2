4dd4a3c726e70c0fff83c9d90fb548ce4a441a70
iurimms <iurimms@gmail.com>
2025-12-11 14:52:30 -0300

    Update DDL.sql
    Criacao das tabelas DIM e FATO, DW.
    parte 2

--- DW/DDL.sql
+++ DW/DDL.sql
@@ -23,3 +23,28 @@ CREATE TABLE dw.dim_DetalhesPartida (
     GameID VARCHAR(50),
     modo_jogo VARCHAR(20),
     lado_time VARCHAR(10),
+    resultado VARCHAR(10),
+    surrender VARCHAR(3)
+);
+
+
+CREATE TABLE dw.fato_Performance (
+    id_fato SERIAL PRIMARY KEY,
+    sk_campeao INT REFERENCES dw.dim_Campeao(sk_campeao),
+    sk_jogador INT REFERENCES dw.dim_Jogador(sk_jogador),
+    sk_detalhes INT REFERENCES dw.dim_DetalhesPartida(sk_detalhes),
+    qtd_kills INT,
+    qtd_deaths INT,
+    qtd_assists INT,
+    kda_ratio NUMERIC(10,2),
+    ouro_ganho INT,
+    ouro_gasto INT,
+    minions_abatidos INT,
+    dano_total_campeoes INT,
+    dano_objetivos INT,
+    dano_recebido INT,
+    dano_mitigado INT,
+    placar_visao INT,
+    wards_colocadas INT,
+    tempo_cc_aplicado INT
+);
